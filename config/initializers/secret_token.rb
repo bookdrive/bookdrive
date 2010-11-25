ec81e90aee81afeb89d378a8534aca0d9bc0a905
Marshall Bessières <mbcoder@me.com>
2010-11-24 23:05:19 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookdrive::Application.config.secret_token = 'b5f4b2456904053f1a8f819d03ebda8d9939009859c6d89a9fccc599e746a6251aac90090aa846d8f8860c209ff3f3d1fb26ae9240a79737e892831a9c091086'
