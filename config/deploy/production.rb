67d92513be559d971fc0ca9e2a09915b290c36e7
Marshall Bessières <mbcoder@me.com>
2010-11-27 21:18:19 -0500

    Updated cap deployment config

---
+++ config/deploy/production.rb
@@ -0,0 +1,3 @@
+set :branch, 'alpha0_3'
+set :deploy_to, "/home/bookdrive/rails"
+set :rails_env, 'production'
---
+++ config/deploy/qa.rb
@@ -0,0 +1,3 @@
+set :branch, 'master'
+set :deploy_to, "/home/bookdrive/rails/qa"
+set :rails_env, 'qa'
