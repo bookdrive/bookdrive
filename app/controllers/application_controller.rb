682b4cc435bbe3e654da0b660a37da95b93a01f9
Marshall Bessières <marshall@bessieres.us>
2010-11-29 02:24:26 -0500

    Fix order_number bug that kicked people out on re-submitting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,17 @@ class ApplicationController < ActionController::Base
   
   def confirm_donor
     confirmation_code = params[:donor_id] || params[:id]
+    if confirmation_code !~ /-\d+-/
+      confirmation_code.sub!(/(\d{3})-?(\d{7})-?(\d{7})/, '\1-\2-\3')
+    end
+    
     @donor = Donor.find_by_confirmation_code( confirmation_code )
     if user_signed_in?
       return
     end
     donor_code = cookies['donor_' + confirmation_code]
+    logger.debug donor_code
+    #logger.debug @donor.donor_code
     if !@donor || !donor_code || donor_code != @donor.donor_code
       redirect_to thankyou_path, :alert => "The order number you entered has already been used on another computer."
     end
