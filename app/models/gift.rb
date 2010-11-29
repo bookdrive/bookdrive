2fe0a11e3905d27c7f380b347fc091cb4886e2eb
Marshall Bessières <mbcoder@me.com>
2010-11-28 23:50:52 -0500

    Freshened up the gifts views so Jack can upload

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,3 +1,9 @@
 class Gift < ActiveRecord::Base
+  validates_presence_of :name, :message => "You must include a name that will appear in the list of downloads"
+#  validates_presence_of :attachment_file_name, :message => "You have to choose a file to upload"
   has_attached_file :attachment
+  validates_attachment_presence :attachment
+  validates_attachment_size :attachment, :less_than => 10.kilobytes 
+  
+  
 end
