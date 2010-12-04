55dc65d020fe9d4d5f7056224c265012af8242a3
Marshall Bessières <mbcoder@me.com>
2010-12-04 00:51:30 -0500

    [Bug 13] Paginate donors list. Also allow searching and sorting. All ajax-driven.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -12,6 +12,13 @@ module LayoutHelper
     @show_title
   end
   
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
+  end
+  
   def stylesheet(*args)
     content_for(:head) { stylesheet_link_tag(*args) }
   end
