500e42602b3d22dc1e756051d8bc4a932ac9cbf9
Marshall Bessières <marshall@bessieres.us>
2010-12-04 22:58:59 -0500

    [Bug 55] Add press articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101204061153) do
+ActiveRecord::Schema.define(:version => 20101205033409) do
 
   create_table "donors", :force => true do |t|
     t.string   "order_number"
@@ -47,6 +47,18 @@ ActiveRecord::Schema.define(:version => 20101204061153) do
     t.string   "attachment_content_type"
     t.integer  "attachment_file_size"
     t.datetime "attachment_updated_at"
+  end
+
+  create_table "press", :force => true do |t|
+    t.string   "title"
+    t.text     "summary"
+    t.string   "source"
+    t.string   "url"
+    t.date     "date"
+    t.string   "author"
+    t.text     "embed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
