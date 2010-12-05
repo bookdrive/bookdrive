12e0976ede454ec90c33c7a746d9ad5207b6e94f
Marshall Bessières <mbcoder@me.com>
2010-12-04 22:27:34 -0500

    [Bug 60] Cache static pages and sweep home when adding donors

---
+++ app/controllers/donor_sweeper.rb
@@ -0,0 +1,27 @@
+class DonorSweeper < ActionController::Caching::Sweeper
+  observe Donor # This sweeper is going to keep an eye on the Product model
+ 
+  # If our sweeper detects that a Donor was created call this
+  def after_create(donor)
+          expire_cache_for(donor)
+  end
+ 
+  # If our sweeper detects that a Donor was updated call this
+  #def after_update(product)
+  #        expire_cache_for(product)
+  #end
+ 
+  # If our sweeper detects that a Donor was deleted call this
+  def after_destroy(donor)
+          expire_cache_for(donor)
+  end
+ 
+  private
+  def expire_cache_for(donor)
+    # Expire the home page now that we added a new donor
+    expire_page(:controller => 'pages', :action => 'home')
+ 
+    # Expire a fragment
+    #expire_fragment('all_available_products')
+  end
+end
