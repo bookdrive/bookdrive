8571feea4bdaf7a64a0aeaed034e589e20873be0
Marshall Bessières <marshall@bessieres.us>
2010-12-07 03:43:09 -0500

    Disable user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter { |c| Authorization.current_user = c.current_user }
+  #before_filter { |c| Authorization.current_user = c.current_user }
   
   protected
 
