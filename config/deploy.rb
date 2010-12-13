f3b5d7e82a5c5fa20fddfb58235d269a1fb83422
Marshall Bessières <mbcoder@me.com>
2010-12-13 01:46:47 -0500

    [#76] Create a custom maintenance page

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,37 @@ namespace :deploy do
     
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end  
+  end
+  
+  namespace :web do
+      desc <<-DESC
+        Present a maintenance page to visitors. Disables your application's web \
+        interface by writing a "maintenance.html" file to each web server. The \
+        servers must be configured to detect the presence of this file, and if \
+        it is present, always display it instead of performing the request.
+
+        By default, the maintenance page will just say the site is down for \
+        "maintenance", and will be back "shortly", but you can customize the \
+        page by specifying the REASON and UNTIL environment variables:
+
+          $ cap deploy:web:disable \\
+                REASON="a hardware upgrade" \\
+                UNTIL="12pm Central Time"
+
+        Further customization will require that you write your own task.
+      DESC
+      task :disable, :roles => :web do
+        require 'erb'
+        on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+
+        reason = ENV['REASON']
+        deadline = ENV['UNTIL']      
+        template = File.read('app/views/pages/maintenance.html.erb')
+        page = ERB.new(template).result(binding)
+
+        put page, "#{shared_path}/system/maintenance.html", :mode => 0644
+      end
+    end
 end
 
 task :backup, :roles => :db, :only => { :primary => true } do
