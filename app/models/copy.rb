b2205fde574da5356ce38650e8a0b1faac63501a
Marshall Bessières <mbcoder@me.com>
2010-12-13 00:10:17 -0500

    [#93 #118] Add multiple copies at once. Delete copies

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -1,4 +1,11 @@
 class Copy < ActiveRecord::Base
   belongs_to :book, :counter_cache => 'copies_delivered'
   belongs_to :user
+  
+  def copy_count
+    return 1
+  end
+  
+  def copy_count=(copy_count)
+  end
 end
