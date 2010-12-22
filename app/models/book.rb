831d064af7e315fe3370107579fc5ccbaa0db757
Marshall Bessières <marshall@bessieres.us>
2010-12-21 22:28:39 -0800

    [#140] Auto-assign book set numbers by parsing the title

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,17 +1,9 @@
 class Book < ActiveRecord::Base
   validates_presence_of :title
-  #validates_presence_of :author
-  #validates_presence_of :amazon_image_width, :unless => "amazon_image_url.nil?||amazon_image_url.empty?"
-  #validates_presence_of :amazon_image_height, :unless => "amazon_image_url.nil?||amazon_image_url.empty?"
-
-  #validates_length_of :author, :minimum => 5
-  #validates_length_of :title, :minimum => 5
   
   validates_numericality_of :amazon_price, :greater_than_or_equal_to => 0, :allow_blank => true
   validates_numericality_of :copies_desired, :greater_than_or_equal_to => 0, :only_integer => true
   validates_numericality_of :copies_received, :greater_than_or_equal_to => 0, :only_integer => true
-  #validates_numericality_of :amazon_image_width, :greater_than => 0, :only_integer => true, :allow_blank => true
-  #validates_numericality_of :amazon_image_height, :greater_than => 0, :only_integer => true, :allow_blank => true
   
   has_many :copies, :dependent => :destroy
   belongs_to :user
@@ -22,6 +14,12 @@ class Book < ActiveRecord::Base
   before_create :calculate_dollars
   before_save :calculate_total_book_count
   before_create :calculate_total_book_count
+  before_save :determine_complete
+  before_create :determine_complete
+  before_save :determine_set
+  before_create :determine_set
+  before_save :trim_refs_from_urls
+  before_create :trim_refs_from_urls
   
   def update_from_wl_book(wl_book)
     wl_book.attribute_hash().each do |key,value|
@@ -52,6 +50,18 @@ class Book < ActiveRecord::Base
   
   private
   
+  def determine_set
+    if self.title =~ /Books 1(?:-| to )(\d+)/ || self.title =~ /(\d+) Book Box Set/
+      self.books_in_set = $1
+    end    
+  end
+  
+  def determine_complete
+    if self.copies_received >= self.copies_desired
+      self.copies_complete = true
+    end
+  end
+  
   def calculate_dollars  
     copies = self.copies_delivered > self.copies_received ? self.copies_delivered : self.copies_received
     price = self.amazon_price && self.amazon_price > 0.00 ? self.amazon_price : 0.00
@@ -62,7 +72,14 @@ class Book < ActiveRecord::Base
     copies = self.copies_delivered > self.copies_received ? self.copies_delivered : self.copies_received
     self.total_book_count = self.books_in_set * copies
   end
-
   
+  def trim_refs_from_urls
+    if self.amazon_product_url.present?
+      self.amazon_product_url.sub!(/ref=.+?\/[\d\-]+/,'')
+    end
+    if self.amazon_wl_cart_url.present?
+      self.amazon_wl_cart_url.sub!(/ref=.+?\/[\d\-]+/,'')
+    end
+  end  
   
 end
