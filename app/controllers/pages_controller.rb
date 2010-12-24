d9d1b4a66f38f9eef973d339e9e925333969b042
Marshall Bessières <mbcoder@me.com>
2010-12-23 22:47:56 -0800

    [#151] Redirect to cookies_required if the session_cookie is not found

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   filter_access_to :all
 
-  caches_page :home, :about, :faq, :usedbooks, :press
+  caches_page :home, :about, :faq, :usedbooks, :press, :cookies
   
   def home
   end
@@ -28,11 +28,44 @@ class PagesController < ApplicationController
 
   def oops
   end
+
+  def cookies_required
+  end
   
   
   private
   
-  MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
+  MOBILE_BROWSERS = [
+    "android",
+    "ipod",
+    "opera mini",
+    "blackberry",
+    "palm",
+    "hiptop",
+    "avantgo",
+    "plucker",
+    "xiino",
+    "blazer",
+    "elaine",
+    "windows ce; ppc;",
+    "windows ce; smartphone;",
+    "windows ce; iemobile",
+    "up.browser",
+    "up.link",
+    "mmp",
+    "symbian",
+    "smartphone",
+    "midp",
+    "wap",
+    "vodafone",
+    "o2",
+    "pocket",
+    "kindle",
+    "mobile",
+    "pda",
+    "psp",
+    "treo"
+  ]
 
   def is_mobile?
     agent = request.env["HTTP_USER_AGENT"].downcase
